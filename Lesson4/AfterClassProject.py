cde97b346571414ac24118cc5e754a6900272c20
royroyalle <21ke21i3387@21kschool.school>
2025-11-09 20:04:02 +0530

    .

--- Lesson4/AfterClassProject.py
+++
@@ -1,3 +0,0 @@
-num = int(input("Please enter a number to check it's number of digits"))
-while num <= 9.9:
-    print(num, "has 1 number of digit")
---
+++ Lesson4/AfterClassProject2.py
@@ -0,0 +1,4 @@
+num = int(input("Please enter a number"))
+numex = int(input("Please enter it's power"))
+expo = num **numex
+print(num, "raised to the power", numex, "is...", expo)
