84be0d4eafdc0b1513af0ab3d363f3e17cca85dc
royroyalle <21ke21i3387@21kschool.school>
2025-11-12 19:44:42 +0530

    .

--- Lesson5/AfterClassProject.py
+++ Lesson5/AfterClassProject.py
@@ -1,1 +1,9 @@
-num = int(input("Please enter a decimal number to find it's binary version"))
+print("Welcome to decimal to binar number calculator")
+decimal = int(input("Please enter a non negative number "))
+temp = decimal
+binary = ""
+while temp > 0:
+         rem = temp % 2
+         binary = str(rem) + binary
+         temp = temp // 2
+print("binary version:", binary)
